8c0c8189a599afb1d96286f3a992a24241451628
Anna Skobtsova <anna.skobtsova@gmail.com>
2018-10-14 23:59:01 +0300

    Update 2018.10.14 Homework 1/Task 6.cpp

---
+++ 2018.10.14 Homework 1/Task 6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+void swap(int& a, int& b)
+{
+	int t = a;
+	a = b;
+	b = t;
+}
+
+void sort(int *p, int N) 
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			if (*(p + i) > *(p + j))
+			{
+				swap(*(p + i), *(p + j));
+			}
+		}
+	}
+}
+
+int main() 
+{
+	int N;
+	cin >> N;
+	int *p = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> *(p + i);
+	};
+	sort(p, arraySize);
+	for (int i = 0; i < N; i++)
+	{
+		cout << *(p + i) << " ";
+	};
+	return 0;
+}
