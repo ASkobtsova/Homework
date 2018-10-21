7d2f1ef3f2b72f31fea51665f5e420b77c4b6fe3
Anna Skobtsova <anna.skobtsova@gmail.com>
2018-10-21 21:12:31 +0300

    Update 2018.10.21 Homework 2/Task 3.cpp

---
+++ 2018.10.21 Homework 2/Task 3.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+struct array
+{
+    int N; //N is array's size
+    int* p;
+    array(int a)
+    {
+        N = a;
+        p = new int[a];
+    };
+    array(const array& A)
+    {
+        N = A.N;
+        p = A.p;
+    };
+
+    int &operator[](int i)
+    {
+        return p[i];
+    };
+
+    ~array()
+    {
+        delete[] p;
+    };
+
+    void in()
+    {
+        for (int i = 0; i < N; i++)
+            cin >> *(p + i);
+    };
+
+    void out()
+    {
+        for (int i = 0; i < N; i++)
+            cout << *(p + i) << " " << endl;
+    };
+
+    int max()
+    {
+        int t = *p;
+        for (int i = 0; i < N; i++)
+        {
+            if (*(p + i) > t)
+            {
+                t = *(p + i);
+            }
+        }
+        return t;
+    };
+    int min()
+    {
+        int t = *p;
+        for (int i = 0; i < N; i++)
+        {
+            if (*(p + i) < t)
+            {
+             t = *(p + i);
+            }
+            }
+        return t;
+
+    };
+
+};
+
+void swap(int *p[])
+{   int i, k;
+    int temp = *p[i];
+	*p[i] = *p[k];
+	*p[k] = temp;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    array X(N);
+    cout << "The elements are:" << endl;
+    X.in();
+    X.out();
+    cout << "The greatest element is " << X.max() << endl;
+    cout << "The lowest element is " << X.min() << endl;
+    swap(X[0], X[1]);
+    cout << "Elements X[0] and X[1] were swapped" << endl;
+    cout << "X[0] = " << X[0] << ", " << "X[1] = "<< X[1];
+    return 0;
+}
