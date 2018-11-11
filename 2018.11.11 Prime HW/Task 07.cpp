b796e2186e3ea9782f48e3a700c965e346d1154e
Anna Skobtsova <anna.skobtsova@gmail.com>
2018-11-11 18:28:56 +0300

    Update 2018.11.11 Prime HW/Task 07.cpp

---
+++ 2018.11.11 Prime HW/Task 07.cpp
@@ -0,0 +1,31 @@
+/*This program has to find all the elements of the sequence that have 7 dividers*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	int *array = new int[N];
+	for (int i = 0; i < N; i++)
+        {
+		cin >> *(array + i);
+        }
+	cout << endl;
+	for (int j = 0; j < N; j++)
+	{
+		int a = 0;
+		int b = *(array + j);
+		for (int j = 1; j <= b; j++)
+		{
+			if ((b % j) == 0)
+			{
+				a += 1;
+			}
+		}
+		if (a == 7)
+		{
+			cout << b << " ";
+		}
+	}
+	return 0;
+}
