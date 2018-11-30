77f047f88c6b8c591dd13774c6675a8284609856
Anna Skobtsova <anna.skobtsova@gmail.com>
2018-11-30 23:27:45 +0300

    Create Task 07.cpp

---
+++ 2018.11.25 Homework 4/Task 07.cpp
@@ -0,0 +1,44 @@
+/*this program has to contain an effective function that writes out from an array of length n all elements whose number of twos in the ternary notation is less than k.
+The function has to return the total number of twos in the ternary record of all the array numbers.
+The procedure is effective because we simply divide by 3 and find the remainder of division. It's the most effective way.
+The effectiveness is O(log3 n)*/
+
+#include <iostream>
+using namespace std;
+
+int out(int *A, int x, int y)
+{
+	int sum = 0;
+	for (int i = 0; i < x; i++)
+	{
+		int twos = 0, m = A[i];
+		while (m > 1)
+		{
+			if ((m % 3) == 2)
+                {
+                twos++;
+				sum++;
+			    }
+			m /= 3;
+		}
+		if (twos < y)
+            {
+            cout << A[i] << " ";
+            };
+	}
+	return sum;
+}
+int main()
+{
+	int n, k;
+	cin >> n;
+	cin >> k;
+	int *p = new int[n];
+	for (int i = 0; i < n; i++)
+    {
+		cin >> p[i];
+	}
+
+	cout << out(p, n, k);
+	return 0;
+}
