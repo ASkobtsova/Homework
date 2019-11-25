fd08a223ffdb01ec7d20ffaa2dc3f892857aecd4
Anna Skobtsova <43076328+ASkobtsova@users.noreply.github.com>
2019-11-26 00:45:02 +0300

    Update Task 01.cpp

--- 2019.11.25 Homework 12/Task 01.cpp
+++ 2019.11.25 Homework 12/Task 01.cpp
@@ -1,6 +1,6 @@
 /*Task: this program has to find such a graph with the least number of edges, which has the power
-of the minimum separating set of 3, the power of the minimum cut of 4,
-and the minimum degree of vertices of 5.
+of the minimum separating set = 3, the power of the minimum cut = 4,
+and the minimum degree of vertices = 5.
 Comment: the graph is minimal in terms of the number of edges,
 because removing any edge changes the minimum degree of the vertex. So, in that case,
 the search which we used to find this graph is complete.*/
